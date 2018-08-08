7844f3f768a0c5c41c16f035d0abc948dc80dc6f
Matthias Meike <matthiasmeike@gmx.de>
2018-08-08 18:58:29 +0200

    Electron 2.0.6 flickering demonstration

---
+++ app/code0.js
@@ -0,0 +1,17 @@
+gdjs.NewSceneCode = {};
+
+gdjs.NewSceneCode.conditionTrue_0 = {val:false};
+gdjs.NewSceneCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.NewSceneCode.eventsList0xaf580 = function(runtimeScene, context) {
+
+}; //End of gdjs.NewSceneCode.eventsList0xaf580
+
+
+gdjs.NewSceneCode.func = function(runtimeScene, context) {
+context.startNewFrame();
+
+gdjs.NewSceneCode.eventsList0xaf580(runtimeScene, context);return;
+}
+gdjs['NewSceneCode']= gdjs.NewSceneCode;
